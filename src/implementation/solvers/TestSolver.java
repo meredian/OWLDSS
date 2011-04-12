ec486a169a3680307f7bcd7fea494b31189dbbb8
Alexey <where-is-s@where-is-s-note.(none)>
2011-04-12 23:09:40 +0700

    [+] Simple TestSolver implementation oriented for RowInvertTask.

--- src/implementation/solvers/TestSolver.java
+++ src/implementation/solvers/TestSolver.java
@@ -3,6 +3,10 @@ package implementation.solvers;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import org.semanticweb.owlapi.model.IRI;
+
+import core.owl.OWLIndividualBuilder;
+import core.owl.OWLIndividualReader;
 import core.owl.OWLOntologyObjectShell;
 import core.owl.objects.Task;
 import core.repository.AbstractSolver;
@@ -27,7 +31,25 @@ public class TestSolver extends AbstractSolver {
 	@Override
 	public boolean solveTaskByMethod(OWLOntologyObjectShell ontologyShell, Task task, MethodSignature method) {
 		// TODO Auto-generated method stub
-		return false;
+		IRI inputIRI = task.getInputObjects().iterator().next();
+		OWLIndividualReader inputReader = ontologyShell.getReader(inputIRI);
+		String input = inputReader.getStringValue("RowValue");
+		String[] inputRow = input.split(" ");
+		String output = new String();
+		for (int i = 0; i < inputRow.length; ++i)
+			output += inputRow[inputRow.length-1 - i] + " ";
+		output = output.substring(0, output.length() - 1); // cut last space
+		
+		OWLIndividualBuilder resultBuilder = ontologyShell.createIndividual("Row");
+		resultBuilder.addAxiom("RowValue", output);
+		
+		OWLIndividualBuilder presentationBuilder = ontologyShell.createIndividual("StringPresentation");
+		presentationBuilder.addAxiom("Value", "Here is the row: " + output);
+		
+		resultBuilder.addObjectAxiom("HasPresentation", presentationBuilder.getIRI());
+		task.getBuilder().addObjectAxiom("HasResult", resultBuilder.getIRI());
+		
+		return true;
 	}
 
 }
