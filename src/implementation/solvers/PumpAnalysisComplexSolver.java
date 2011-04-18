9e2cb6d2c93c1fc37c6eb75d4dc8082f9ca0a115
Anton Sidelnikov <ndmeredian@gmail.com>
2011-04-18 18:05:36 +0700

    [-] Added new tasks to original task output

--- src/implementation/solvers/PumpAnalysisComplexSolver.java
+++ src/implementation/solvers/PumpAnalysisComplexSolver.java
@@ -1,8 +1,5 @@
 package implementation.solvers;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.semanticweb.owlapi.model.IRI;
 
 import core.owl.OWLOntologyObjectShell;
@@ -11,13 +8,13 @@ import core.repository.AbstractSolver;
 import core.repository.MethodSignature;
 
 public class PumpAnalysisComplexSolver extends AbstractSolver {
-	
+
 	private static final String METHOD_NAME = "Standard";
 	private static final String FIRST_TASK_CLASS = "RowTendencyAnalysisTask";
 	private static final String SECOND_TASK_CLASS = "PumpTendencyAnalysisTask";
-	
+
 	private static final String FIRST_INPUT = "PumpEfficiencyDeviationRow";
-	
+
 	public PumpAnalysisComplexSolver() {
 		init();
 		MethodSignature efficiencyTrendAnalysis = new MethodSignature(METHOD_NAME);
@@ -26,36 +23,34 @@ public class PumpAnalysisComplexSolver extends AbstractSolver {
 
 	@Override
 	public boolean solveTaskByMethod(OWLOntologyObjectShell ontologyShell, Task task, MethodSignature method) {
-		
-		if ( method.getName().equals(METHOD_NAME)) {
-			splitTask( ontologyShell, task );
+
+		if (method.getName().equals(METHOD_NAME)) {
+			splitTask(ontologyShell, task);
 			return true;
 		}
 		return false;
 	}
 
-	private Set<Task> splitTask(OWLOntologyObjectShell ontologyShell, Task task) {
+	private void splitTask(OWLOntologyObjectShell ontologyShell, Task task) {
 		IRI rowAnalysisTaskIRI = ontologyShell.createIndividual(FIRST_TASK_CLASS).getIRI();
 		IRI pumpAnalysisTaskIRI = ontologyShell.createIndividual(SECOND_TASK_CLASS).getIRI();
-		
-		Task rowAnalysisTask =  new Task(ontologyShell.getOWLNamedIndividual(rowAnalysisTaskIRI), 
-				ontologyShell.getBuilder(rowAnalysisTaskIRI),
-				ontologyShell.getReader(rowAnalysisTaskIRI), ontologyShell);
-		
+
+		Task rowAnalysisTask = new Task(ontologyShell.getOWLNamedIndividual(rowAnalysisTaskIRI),
+				ontologyShell.getBuilder(rowAnalysisTaskIRI), ontologyShell.getReader(rowAnalysisTaskIRI),
+				ontologyShell);
+
 		rowAnalysisTask.addInput(task.getImportedObjectsByClass(FIRST_INPUT));
-		
-		Task pumpAnalysisTask =  new Task(ontologyShell.getOWLNamedIndividual(pumpAnalysisTaskIRI), 
-				ontologyShell.getBuilder(pumpAnalysisTaskIRI),
-				ontologyShell.getReader(pumpAnalysisTaskIRI), ontologyShell);
+
+		Task pumpAnalysisTask = new Task(ontologyShell.getOWLNamedIndividual(pumpAnalysisTaskIRI),
+				ontologyShell.getBuilder(pumpAnalysisTaskIRI), ontologyShell.getReader(pumpAnalysisTaskIRI),
+				ontologyShell);
 
 		pumpAnalysisTask.addInput(task.getInputObjects());
 		pumpAnalysisTask.addImport(task.getImportedObjects());
 		pumpAnalysisTask.addSubTask(rowAnalysisTask);
-		
-		Set<Task> result = new HashSet<Task>(2);
-		result.add(rowAnalysisTask);
-		result.add(pumpAnalysisTask);
-		return result;
+
+		task.addOutputObject(rowAnalysisTaskIRI);
+		task.addOutputObject(pumpAnalysisTaskIRI);
 	}
 
 }
