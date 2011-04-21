a2f4a13a3cd7df7246e9d65e23191d5522921864
Anton Sidelnikov <ndmeredian@gmail.com>
2011-04-22 01:36:41 +0700

    [+] Add config for Zagorulko G.B. to repo

---
+++ ruby/config/zagorulko.rb
@@ -0,0 +1,59 @@
+OWLDSS::RepoManipulator.set do
+
+  solver("implementation.solvers.SempSolver") {
+
+    method("EfficiencyTrendAnalysis"){
+      param "MODULE_PATH", "\\Work\\SempN\\modules\\EfficiencyTrendAnalysis\\"
+      param "MODULE_LAUNCHER", "EfficiencyTrendAnalysis_Launcher.pm"
+      param "MODULE_DATA_INPUT",
+        "\\Work\\SempN\\EfficiencyTrendAnalysis\\EfficiencyTrendAnalysis_CreateData.pm"
+      param "MODULE_CREATE_DATA_HEADER",
+       "uses CATNemNumbers, CATSempTypes, CATSempProductions, CATSempContainers;\r\n" +
+       "uses Global_Ontology;\r\n" +
+       "rule CreateData\r\n" +
+       "=>\r\n" +
+       "new\r\n"
+      param "MODULE_CREATE_DATA_TAIL","end;\r\n"
+    }
+
+    method("RowTendencyAnalysis"){
+      param "MODULE_PATH", "\\Work\\SempN\\modules\\RowTendencyAnalysis\\"
+      param "MODULE_LAUNCHER", "RowTendencyAnalysis_Launcher.pm"
+      param "MODULE_DATA_INPUT",
+        "\\Work\\SempN\\modules\\RowTendencyAnalysis\\RowTendencyAnalysis_CreateData.pm"
+      param "MODULE_CREATE_DATA_HEADER",
+       "uses CATNemNumbers, CATSempTypes, CATSempProductions, CATSempContainers;\r\n" +
+       "uses Global_Ontology;\r\n" +
+       "rule CreateData\r\n" +
+       "=>\r\n" +
+       "new\r\n"
+      param "MODULE_CREATE_DATA_TAIL","end;\r\n"
+    }
+
+    method("PumpTendencyAnalysis"){
+      param "MODULE_PATH", "\\Work\\SempN\\modules\\PumpTendencyAnalysis\\"
+      param "MODULE_LAUNCHER", "PumpTendencyAnalysis_Launcher.pm"
+      param "MODULE_DATA_INPUT",
+        "\\Work\\SempN\\modules\\PumpTendencyAnalysis\\PumpTendencyAnalysis_CreateData.pm"
+      param "MODULE_CREATE_DATA_HEADER",
+        "uses CATNemNumbers, CATSempTypes, CATSempProductions, CATSempContainers;\r\n" +
+        "uses Global_Ontology;\r\n" +
+       "rule CreateData\r\n" +
+       "=>\r\n" +
+       "new\r\n"
+      param "MODULE_CREATE_DATA_TAIL","end;\r\n"
+    }
+
+  }
+
+  solver("implementation.solvers.PumpAnalysisComplexSolver") {
+    method("Standard"){
+    }
+  }
+
+  solver("implementation.solvers.RowTendencySimpleAnalyser") {
+    method("Standard"){
+    }
+  }
+
+end
